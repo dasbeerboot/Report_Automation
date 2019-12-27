df2d72c78ebf4e9803e0453f3c3be6c56ac7efa6
Juwon Chun <juwonchun@gmail.com>
2019-12-27 13:18:12 +0900

    working fine

--- index.py
+++ index.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager 
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.action_chains import ActionChains
 
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
@@ -19,42 +18,23 @@ loginBtn = driver.find_element_by_xpath('/html/body/div/div[2]/div/div/div/div[2
 
 loginBtn.send_keys(Keys.RETURN)
 
-while True:
-    reportNum = driver.find_element_by_xpath('/html/body/div/div[1]/div/div[4]/form/div[1]/div[1]/select')
-    rNum = input ('10, 20, 50, 100, 200 : ')
-    reportNum.click()
-    reportNum.send_keys(rNum)
+reportNum = driver.find_element_by_xpath('/html/body/div/div[1]/div/div[4]/form/div[1]/div[1]/select')
+rNum = input ('20, 50, 100, 200 : ') #If you want 10, jsut go click it with your finger >:p
+reportNum.click()
+reportNum.send_keys(rNum)
 
-    reportProject = driver.find_element_by_xpath('/html/body/div/div[1]/div/div[4]/form/div[1]/div[2]/select')
-    rPro = int(input ('Full Body -- Chest == 1 \nFull Body -- Spine == 2\nChoose menu : '))
+reportProject = driver.find_element_by_xpath('/html/body/div/div[1]/div/div[4]/form/div[1]/div[2]/select')
+rPro = int(input ('Full Body -- Chest == 1 \nFull Body -- Spine == 2\nChoose menu : '))
 
-    if rPro is 1:
-        driver.find_element_by_xpath('/html/body/div/div[1]/div/div[4]/form/div[1]/div[2]/select/option[10]').click()
-    elif rPro is 2:
-        driver.find_element_by_xpath('/html/body/div/div[1]/div/div[4]/form/div[1]/div[2]/select/option[13]').click()
+if rPro is 1:
+    driver.find_element_by_xpath('/html/body/div/div[1]/div/div[4]/form/div[1]/div[2]/select/option[10]').click()
+elif rPro is 2:
+    driver.find_element_by_xpath('/html/body/div/div[1]/div/div[4]/form/div[1]/div[2]/select/option[13]').click()
 
-    driver.find_element_by_xpath('/html/body/div/div[1]/div/div[4]/form/div[3]/div/button').send_keys(Keys.RETURN)
-    
-    rValue = int(input('Select from 1 to 4 : '))
+driver.find_element_by_xpath('/html/body/div/div[1]/div/div[4]/form/div[3]/div/button').send_keys(Keys.RETURN)
 
-    slider = driver.find_element_by_xpath('/html/body/div/div/div[2]/form/div/div[1]/div[2]/div/input')
-    move = ActionChains(driver)
-    
-    temp = 0
 
-    while True:             
-        if rValue is 1:
-            temp = int(25)
-        elif rValue is 2:
-            temp = int(50)
-        elif rValue is 3: 
-            temp = int(75)
-        elif rValue is 4:
-            temp = int(100)
-        else:
-            print('Please select from 1 to 4')
-            pass
-
-        move.click_and_hold(slider).move_by_offset(temp, 3).release().perform()
-
-        driver.find_element_by_xpath('/html/body/div/div/div[2]/form/div/div[4]/button').send_keys(Keys.RETURN)
+while True: 
+    rValue = int(input('Select from 0 to 3 : '))
+    driver.find_element_by_xpath('/html/body/div/div/div[2]/form/div/div[1]/div[2]/div/input').__setattr__("value", rValue)
+    driver.find_element_by_xpath('/html/body/div/div/div[2]/form/div/div[4]/button').send_keys(Keys.RETURN)
